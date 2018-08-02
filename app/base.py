cb97c7614a1fa5c467c444295bb3ed82b72a95d4
Jawaad Mahmood <jawaad.mahmood@gmail.com>
2018-08-03 01:05:53 +0900

    Added Bidding between players minigame

--- app/base.py
+++ app/base.py
@@ -21,6 +21,7 @@ class PrivateCompany:
         self.revenue = None
         self.belongs_to = None
         self.player_bids: List[PlayerBid] = None
+        self.passed_by: List[Player] = None
         self.pass_count = None
 
     @staticmethod
@@ -39,6 +40,7 @@ class PrivateCompany:
                  belongs_to: "Player" = None,
                  actual_cost: int = None,
                  player_bids: List[PlayerBid] = None,
+                 passed_by: List[Player] = None,
                  pass_count: int = 0) -> "PrivateCompany":
         pc = PrivateCompany()
         pc.order = order
@@ -50,6 +52,7 @@ class PrivateCompany:
         pc.base = base
         pc.belongs_to = belongs_to
         pc.player_bids = [] if player_bids is None else player_bids
+        pc.passed_by = [] if passed_by is None else passed_by
         pc.pass_count = pass_count
 
         return pc
@@ -60,10 +63,10 @@ class PrivateCompany:
     def hasBids(self) -> bool:
         return len(self.player_bids) > 0
 
-    def passed(self):
+    def passed(self, player: Player):
         self.pass_count += 1
 
-    def modify_price(self, player_count):
+    def reduce_price(self, player_count):
         if self.pass_count % player_count == 0 and self.pass_count > 0:
             self.actual_cost -= 5
 
@@ -74,6 +77,9 @@ class PrivateCompany:
     def belongs(self, player: Player):
         """No security at this level.  If you run this, any bid will be accepted."""
         self.belongs_to(player)
+
+    def set_actual_cost(self, actual_cost):
+        self.actual_cost = actual_cost
 
 
 class Move:
--- app/minigames/private_companies.py
+++ app/minigames/private_companies.py
@@ -18,6 +18,64 @@ class BuyPrivateCompanyMove(Move):
     bid_amount: int
 
 
+class BiddingForPrivateCompany(Minigame):
+    def errors(self) -> List[str]:
+        pass
+
+    def validate_bid(self, move: BuyPrivateCompanyMove):
+        # User needs to be one of the users who bid already.
+        # User needs to not have passed on this company yet.
+        pass
+
+    def validate_pass(self, move: BuyPrivateCompanyMove):
+        # User needs to be one of the users who bid already.
+        # You can't pass if you are the last user with a bid.
+        pass
+
+    def validate_sold(self, move: BuyPrivateCompanyMove):
+        all_bidders = set([player_bid.player for player_bid in move.private_company.player_bids])
+        all_passers = set([pc.passed_by for pc in move.private_company.passed_by])
+
+        if len(all_bidders - all_passers) == 1:
+            purchaser = list(all_bidders - all_passers)[0]
+            actual_cost_of_company = max([player_bid.bid_amount for player_bid in move.private_company.player_bids
+                 if player_bid.player == purchaser]) # Player buys for max amount he bid.
+
+            move.private_company.set_actual_cost(actual_cost_of_company)
+            move.private_company.belongs(move.player)
+            return True
+        return False
+
+    def run(self, move: BuyPrivateCompanyMove, **kwargs) -> bool:
+
+        if BidType.BID == BidType(move.bid_type):
+            if self.validate_bid(move):
+                move.private_company.bid(move.player, move.bid_amount)
+                self.validate_sold(move)
+                return True
+
+        if BidType.PASS == BidType(move.bid_type):
+            if self.validate_pass(move):
+                move.private_company.passed(move.player)
+                self.validate_sold(move)
+                return True
+
+
+        return True
+
+    def next(self, **kwargs) -> str:
+        private_companies: List[PrivateCompany] = kwargs.get('private_companies')
+
+        for pc in private_companies:
+            if not pc.hasOwner() and not pc.hasBids():
+                return "BuyPrivateCompany"
+
+            if not pc.hasOwner() and pc.hasBids():
+                return "BiddingForPrivateCompany"
+
+        return "BuyStock"
+
+
 class BuyPrivateCompany(Minigame):
     """
     Used for the initial auction round of Private Companies
@@ -52,8 +110,8 @@ class BuyPrivateCompany(Minigame):
 
         if BidType.PASS == BidType(move.bid_type):
             if self.validate_pass(move): # You cannot pass on a free company.
-                move.private_company.passed()
-                move.private_company.modify_price(len(kwargs.get('Players')))
+                move.private_company.passed(move.player)
+                move.private_company.reduce_price(len(kwargs.get('Players')))
                 return True
 
         return False
@@ -64,6 +122,7 @@ class BuyPrivateCompany(Minigame):
         for pc in private_companies:
             if not pc.hasOwner() and not pc.hasBids():
                 return "BuyPrivateCompany"
+
             if not pc.hasOwner() and pc.hasBids():
                 return "BiddingForPrivateCompany"
 
